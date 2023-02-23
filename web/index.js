3ec054f389afdf2de98f305b9f5620816f0f1fe3
IntrntSrfr <aleksander@tosbakken.com>
2023-02-23 19:57:37 +0100

    added touch + responsiveness

--- web/index.js
+++ web/index.js
@@ -1,7 +1,7 @@
 const color_scale = chroma.scale(['yellow', 'lightgreen', '008ae5']);
 
 window.onload = () => {
-    let predictionDiv = document.getElementById('predictions')
+    let predictionDiv = document.getElementById('prediction-bars')
     let clearBtn = document.getElementById('clearBtn')
 
     let canvas = document.getElementById('canvas')
@@ -18,7 +18,7 @@ window.onload = () => {
         prevX = currX;
         prevY = currY;
         currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
+        currY = e.clientY - canvas.offsetTop + scrollY;
         draw(ctx,
             prevX,
             prevY,
@@ -32,7 +32,7 @@ window.onload = () => {
         prevX = currX;
         prevY = currY;
         currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
+        currY = e.clientY - canvas.offsetTop + scrollY;
     })
 
     canvas.addEventListener('mouseup', () => {
@@ -43,12 +43,68 @@ window.onload = () => {
         drawFlag = false
     })
 
+    // All touch functionality below comes from following
+    // https://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
+
+    // Set up touch events for mobile, etc
+    canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    canvas.addEventListener("touchend", function (e) {
+        var mouseEvent = new MouseEvent("mouseup", {});
+        canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    canvas.addEventListener("touchmove", function (e) {
+        var touch = e.touches[0];
+        var mouseEvent = new MouseEvent("mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        });
+        canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    // Get the position of a touch relative to the canvas
+    function getTouchPos(canvasDom, touchEvent) {
+        //var rect = canvasDom.getBoundingClientRect();
+        return {
+            x: touchEvent.touches[0].clientX,// - canvas.offsetLeft,
+            y: touchEvent.touches[0].clientY// - canvas.offsetTop
+        };
+    }
+
+    // Prevent scrolling when touching the canvas
+    document.body.addEventListener("touchstart", function (e) {
+        if (e.target === canvas) {
+            e.preventDefault();
+        }
+    }, false);
+    document.body.addEventListener("touchend", function (e) {
+        if (e.target === canvas) {
+            e.preventDefault();
+        }
+    }, false);
+    document.body.addEventListener("touchmove", function (e) {
+        if (e.target === canvas) {
+            e.preventDefault();
+        }
+    }, false);
+
     clearBtn.onclick = () => {
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.width)
         scaleBars(Array(10).fill(0.1))
     }
 
     const draw = (ctx, fromX, fromY, toX, toY) => {
+        console.log(fromX, fromY, toX, toY);
+        
         let scale = ctx.canvas.clientHeight / ctx.canvas.height
         ctx.beginPath();
         ctx.moveTo(fromX / scale, fromY / scale);
@@ -72,12 +128,12 @@ window.onload = () => {
         setTimeout(() => {
             allowPredict = true
         }, 150);
-        
+
         let blob = await generateBlob()
         let form = new FormData()
         form.append("image", blob)
 
-        let resp = await fetch('http://localhost:5000/predict', {
+        let resp = await fetch('https://draw.intrntsr.fr/api/predict', {
             method: 'POST',
             body: form
         })
@@ -86,7 +142,7 @@ window.onload = () => {
     }
 
     const scaleBars = (nums) => {
-        for(let i = 0; i < predictionDiv.children.length; i++){
+        for (let i = 0; i < predictionDiv.children.length; i++) {
             let col = predictionDiv.children[i].children[0]
             col.style.transform = `scaleY(${nums[i]})`
             col.style.backgroundColor = color_scale(nums[i])
